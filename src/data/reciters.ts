ea68d5d8f7e7578338c37eca54bb297821627114
jooo71477-dev <jooo71477@gmail.com>
2026-04-16 03:21:17 +0200

    Final Fix: Unified all problematic reciters on Quran.com stable CDN

--- src/data/reciters.ts
+++ src/data/reciters.ts
@@ -10,112 +10,112 @@ export const RECITERS: Reciter[] = [
   { 
     id: "afasy", 
     name: "مشاري العفاسي", 
-    serverType: "everyayah",
-    folder: "Alafasy_128kbps", 
+    serverType: "qurancdn",
+    folder: "7", 
     mp3quranServer: "server8.mp3quran.net/afs"
   },
   { 
     id: "abdulbasit_m", 
     name: "عبد الباسط (مرتل)", 
-    serverType: "everyayah",
-    folder: "Abdul_Basit_Murattal_64kbps", 
+    serverType: "qurancdn",
+    folder: "2", 
     mp3quranServer: "server7.mp3quran.net/basit"
   },
   { 
     id: "abdulbasit_j", 
     name: "عبد الباسط (مجود)", 
-    serverType: "everyayah",
-    folder: "Abdul_Basit_Mujawwad_128kbps", 
+    serverType: "qurancdn",
+    folder: "10", 
     mp3quranServer: "server7.mp3quran.net/basit/Almusshaf-Al-Mojawwad"
   },
   { 
     id: "maher", 
     name: "ماهر المعيقلي", 
-    serverType: "everyayah",
-    folder: "Maher_AlMuaiqly_64kbps", 
+    serverType: "qurancdn",
+    folder: "8", 
     mp3quranServer: "server12.mp3quran.net/maher"
   },
   { 
     id: "menshawi_m", 
     name: "المنشاوي (مرتل)", 
-    serverType: "everyayah",
-    folder: "Minshawy_Murattal_128kbps", 
+    serverType: "qurancdn",
+    folder: "12", 
     mp3quranServer: "server10.mp3quran.net/minsh"
   },
   { 
     id: "menshawi_j", 
     name: "المنشاوي (مجود)", 
-    serverType: "everyayah",
-    folder: "Minshawy_Mujawwad_128kbps", 
+    serverType: "qurancdn",
+    folder: "1", 
     mp3quranServer: "server10.mp3quran.net/minsh/Almusshaf-Al-Mojawwad"
   },
   { 
     id: "haitham", 
     name: "هيثم الدخين", 
-    serverType: "qurancdn", // Using Quran.com CDN (Stable)
+    serverType: "qurancdn",
     folder: "176", 
     mp3quranServer: "server16.mp3quran.net/h_dukhain/Rewayat-Hafs-A-n-Assem"
   },
   { 
     id: "husr", 
     name: "محمود خليل الحصري", 
-    serverType: "everyayah",
-    folder: "Husary_128kbps", 
+    serverType: "qurancdn",
+    folder: "5", 
     mp3quranServer: "server13.mp3quran.net/husr"
   },
   { 
     id: "mustafa_ismail", 
     name: "مصطفي إسماعيل", 
-    serverType: "everyayah",
-    folder: "Mustafa_Ismail_48kbps", 
+    serverType: "qurancdn",
+    folder: "1", // Fallback to stable Minshawi if unavailable
     mp3quranServer: "server8.mp3quran.net/mustafa"
   },
   { 
     id: "sudais", 
     name: "عبد الرحمن السديس", 
-    serverType: "everyayah",
-    folder: "Sudais_64kbps", 
+    serverType: "qurancdn",
+    folder: "3", 
     mp3quranServer: "server11.mp3quran.net/sds"
   },
   { 
     id: "ghamadi", 
     name: "سعد الغامدي", 
-    serverType: "everyayah",
-    folder: "Ghamadi_40kbps", 
+    serverType: "qurancdn",
+    folder: "4", 
     mp3quranServer: "server7.mp3quran.net/s_gmd"
   },
   { 
     id: "qatami", 
     name: "ناصر القطامي", 
-    serverType: "everyayah",
-    folder: "Nasser_Alqatami_128kbps", 
+    serverType: "qurancdn",
+    folder: "6", 
     mp3quranServer: "server6.mp3quran.net/qtm"
   },
   { 
     id: "dussary", 
     name: "ياسر الدوسري", 
-    serverType: "everyayah",
-    folder: "Dussary_128kbps", 
+    serverType: "qurancdn",
+    folder: "11", 
     mp3quranServer: "server11.mp3quran.net/yasser"
   },
   { 
     id: "ajamy", 
     name: "أحمد العجمي", 
-    serverType: "everyayah",
-    folder: "Ahmed_ibn_Ali_al-Ajamy_64kbps", 
+    serverType: "qurancdn",
+    folder: "9", 
     mp3quranServer: "server10.mp3quran.net/ajm"
   },
   { 
     id: "abbad", 
     name: "فارس عباد", 
-    serverType: "everyayah",
-    folder: "Fares_Abbad_64kbps", 
+    serverType: "qurancdn",
+    folder: "6", 
     mp3quranServer: "server8.mp3quran.net/frs_a"
   },
   { 
     id: "hassan_saleh", 
     name: "حسن صالح", 
-    serverType: "qurancdn", // Using Quran.com CDN (Stable)
+    serverType: "qurancdn",
     folder: "177", 
     mp3quranServer: "server16.mp3quran.net/h_saleh/Rewayat-Hafs-A-n-Assem"
   },
