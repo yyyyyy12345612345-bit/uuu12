36a28d49b1ac0455cfd3d467237af579cf4a1d47
jooo71477-dev <jooo71477@gmail.com>
2026-04-21 18:35:34 +0200

    delete redundant page to fix specificity error

--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import CatchAllPage, { dynamic as dynamicConfig, dynamicParams as paramsConfig } from "./[[...slug]]/page";
-
-export const dynamic = dynamicConfig;
-export const dynamicParams = paramsConfig;
-
-export default CatchAllPage;
